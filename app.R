ba843edc885ef4837cd336d1e6a5404df1872cdb
Hansen Johnson <hjohnson@neaq.org>
2026-02-16 14:43:28 -0500

    init commit

---
+++ app.R
@@ -0,0 +1,220 @@
+# app.R
+library(shiny)
+library(shinydashboard)
+library(ggplot2)
+library(plotly)
+library(tidyverse)
+library(lubridate)
+library(DT)
+
+# --- UI ---
+
+# ui ----------------------------------------------------------------------
+
+ui <- dashboardPage(
+  dashboardHeader(title = "NOMO: Noise Monitoring System", titleWidth = 400),
+  
+  # dashboardSidebar(disable = TRUE),  # Sidebar removed
+  
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Controls", tabName = "controls", icon = icon("sliders")),
+      selectInput(inputId = "dep", label = "Select deployments to display", choices = "All", selectize = T, multiple = T)
+    )
+  ),
+  
+  dashboardBody(
+    fluidRow(
+      box(
+        title = "Timeseries",
+        status = "primary",
+        solidHeader = TRUE,
+        width = 12,
+        collapsible = TRUE,
+        plotlyOutput("plot", height = "400px")
+      )
+    ),
+    fluidRow(
+      box(
+        title = "Hourly",
+        status = "primary",
+        solidHeader = TRUE,
+        width = 6,
+        collapsible = TRUE,
+        plotlyOutput("hourly", height = "400px")
+      ),
+      box(
+        title = "Comparison",
+        status = "primary",
+        solidHeader = TRUE,
+        width = 6,
+        collapsible = TRUE,
+        plotlyOutput("comparison", height = "400px")
+      )
+    ),
+    fluidRow(
+      box(
+        title = "Data Table",
+        status = "info",
+        solidHeader = TRUE,
+        width = 12,
+        collapsible = TRUE,
+        downloadButton("downloadData", "Download"),
+        p(),
+        DTOutput("table")
+      )
+    )
+  )
+)
+
+# server ------------------------------------------------------------------
+
+server <- function(input, output, session) {
+  
+
+  # read in data ------------------------------------------------------------
+
+  # Read text files from server
+  flist=list.files(path = 'data/raw/NOMO-01', pattern = "*.txt", full.names = T, recursive = T)
+  out=vector('list', length = length(flist))
+  for(ii in seq_along(flist)){
+    out[[ii]]=read.delim(flist[ii],sep = ",",header = F)
+  }
+  data = bind_rows(out)
+  colnames(data) = c('id', 'tstamp','dB')
+  data = data %>%
+    mutate(
+      time_utc = as.POSIXct(tstamp, format = '%Y-%m-%d_%H%M%SZ', tz = 'UTC'),
+      time_local = with_tz(time_utc, tzone = 'America/New_York'),
+      dB = as.numeric(dB),
+      dep_id = str_split(flist, pattern = '/', simplify = T)[,4]) %>%
+    arrange(time_utc) %>%
+    select(-tstamp) %>%
+    tibble()
+  
+  print(summary(data))
+  
+  # Dynamically create deployment selectors based on CSV columns
+  observe({
+    dep_id_choices = c('All', unique(data$dep_id))
+    
+    updateSelectInput(
+      session,
+      inputId = "dep",
+      choices = dep_id_choices,
+      selected = 'All'
+    )
+  })
+  
+  observe({
+    print(input$dep)  
+  })
+  
+  d <- reactive({
+    if("All" %in% input$dep){
+      data
+    } else {
+      data %>% filter(dep_id %in% input$dep)
+    }
+  })
+  
+  # plot timeseries ---------------------------------------------------------
+
+  # Render the plotly plot
+  output$plot <- renderPlotly({
+    
+    # convert reactive data object
+    df = d()
+    
+    # plot
+    p <- ggplot(df, aes(x = time_local, y = dB, color = dep_id, group = dep_id)) +  
+      geom_path() +
+      geom_point() +
+      labs(x = 'Time (Eastern)', y = 'Noise level (dB)', color = 'Deployment', group = 'Deployment') +
+      theme_minimal()
+    
+    ggplotly(p)
+  })
+  
+  # plot comparison ---------------------------------------------------------
+
+  # Render the plotly plot
+  output$comparison <- renderPlotly({
+    
+    # convert reactive data object
+    df = d()
+    
+    # plot
+    p <- ggplot(df) +  
+      geom_violin(aes(x = dep_id, y = dB, fill = dep_id, group = dep_id, color = dep_id), alpha = 0.2)+
+      geom_boxplot(aes(x = dep_id, y = dB, fill = dep_id, group = dep_id), outlier.fill = NULL, outlier.shape = 1, width = .25)+
+      labs(x = 'Deployment', y = 'Noise level (dB)') +
+      theme_minimal()+
+      theme(legend.position = "none")
+    ggplotly(p)
+  })
+  
+  # plot hourly -------------------------------------------------------------
+
+  # Render the hourly plot
+  output$hourly <- renderPlotly({
+    
+    # convert reactive data object
+    df = d()
+    
+    # add local time of day
+    df$local_hour = lubridate::hour(df$time_local)
+    
+    # compute summary stats
+    df_sum = df %>% group_by(local_hour, dep_id) %>%
+      summarize(
+        mean_dB = median(dB),
+        min_dB = quantile(dB, probs = 0.25),
+        max_dB = quantile(dB, probs = 0.75)
+      )
+    
+    # plot
+    p <- ggplot() +  
+      geom_point(data = df, aes(x = local_hour, y = dB, color = dep_id), shape = 1) +
+      geom_ribbon(data = df_sum, aes(x = local_hour, ymin = min_dB, ymax = max_dB, color = NULL, fill = dep_id, group = dep_id), alpha = 0.3)+
+      geom_line(data = df_sum, aes(x = local_hour, y = mean_dB, color = dep_id, group = dep_id))+
+      labs(x = 'Hour of day (Eastern)', y = 'Noise level (dB)', color = 'Deployment', group = 'Deployment', fill = 'Deployment') +
+      theme_minimal()
+    ggplotly(p)
+  })
+  
+  # plot table --------------------------------------------------------------
+
+  # Render the filterable table
+  output$table <- renderDT({
+    df <- d() %>%
+      mutate(
+        time_utc = format(time_utc, "%Y-%m-%d %H:%M:%S"),
+        time_local = format(time_local, "%Y-%m-%d %H:%M:%S")
+      ) %>%
+      arrange(desc(time_local))
+    datatable(df, filter = "top", options = list(pageLength = 25), rownames = F)
+  })
+
+  # data download -----------------------------------------------------------
+
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      "nomo-data.csv"
+    },
+    content = function(file) {
+      
+      df <- d() %>%
+        mutate(
+          time_utc = format(time_utc, "%Y-%m-%d %H:%M:%S"),
+          time_local = format(time_local, "%Y-%m-%d %H:%M:%S")
+        ) %>%
+        arrange(desc(time_local))
+        
+      write.csv(df, file)
+    }
+  )
+  
+}
+
+shinyApp(ui, server)
---
+++ readme.md
@@ -0,0 +1,14 @@
+# NoMo App
+R Shiny application for displaying real-time noise data from a NOMO unit
+
+## Summary
+This web application, built using the R Shiny package, serves to display data from our custom-made NOise MOnitors (NOMOs). See the nomo repository (https://github.com/hansenjohnson/nomo) for more details and source code.
+
+## To do
+- Password protection
+- Allow user to define deployments and update notes
+
+## Project organization
+`app.R` = source code for shiny application
+`data` = noise data
+

38e0dc6b0a35f6c66dd70680a6c47cfb93e2a470
Hansen Johnson <hjohnson@neaq.org>
2026-03-17 08:02:52 -0400

    switch to quincy dep

--- src/move_noise_data.sh
+++ src/move_noise_data.sh
@@ -16,7 +16,7 @@ fi
 
 # --- DESTINATION RELATIVE TO SOURCE ---
 SOURCE_DIR="${PROJ_DIR}/data/raw/NOMO-01"
-DEST_DIR="${SOURCE_DIR}/2026-03-07_coral"
+DEST_DIR="${SOURCE_DIR}/2026-03-16_quincy"
 
 mkdir -p "$DEST_DIR"
 

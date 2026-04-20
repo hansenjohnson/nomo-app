a3b9a4f524e99ed10d3ba856fbaf888458a4b6dd
Hansen Johnson <hjohnson@neaq.org>
2026-04-20 16:29:35 -0400

    update for new neaq bench test

--- src/move_noise_data.sh
+++ src/move_noise_data.sh
@@ -16,7 +16,7 @@ fi
 
 # --- DESTINATION RELATIVE TO SOURCE ---
 SOURCE_DIR="${PROJ_DIR}/data/raw/NOMO-01"
-DEST_DIR="${SOURCE_DIR}/2026-04-17_testing"
+DEST_DIR="${SOURCE_DIR}/2026-04-20_bench"
 
 mkdir -p "$DEST_DIR"
 

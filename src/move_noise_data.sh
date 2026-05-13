03bc6215f9297799f7d6049e293f8543c515dc53
Hansen Johnson <hjohnson@neaq.org>
2026-05-13 17:35:39 -0400

    nomo-01 bench testing

--- src/move_noise_data.sh
+++ src/move_noise_data.sh
@@ -16,7 +16,7 @@ fi
 
 # --- NOMO-01 FILES ---
 SOURCE_DIR="${PROJ_DIR}/data/raw/NOMO-01"
-DEST_DIR="${SOURCE_DIR}/2026-04-28_testing"
+DEST_DIR="${SOURCE_DIR}/2026-05-13_testing"
 
 mkdir -p "$DEST_DIR"
 
